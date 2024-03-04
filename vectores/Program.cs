412c5883d98e8b81f000640b96be585a5385695f
eapch <eapc.gom@gmail.com>
2024-03-03 23:44:48 -0400

    primer comentario

---
+++ vectores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+ static void Main()
+    {
+        // Vector de números
+        int[] numeros = new int[] { 1, 2, 3, 4, 5 };
+
+        // Vector de letras
+        char[] letras = new char[] { 'a', 'b', 'c', 'd', 'e' };
+
+        // Vector de palabras
+        string[] palabras = new string[] { "Hola", "Mundo", "es", "genial" };
+
+        // Imprimir vector de números
+        Console.WriteLine("Vector de números:");
+        ImprimirVector(numeros);
+
+        // Imprimir vector de letras
+        Console.WriteLine("\nVector de letras:");
+        ImprimirVector(letras);
+
+        // Imprimir vector de palabras
+        Console.WriteLine("\nVector de palabras:");
+        ImprimirVector(palabras);
+    }
+
+    static void ImprimirVector<T>(T[] vector)
+    {
+        foreach (T elemento in vector)
+        {
+            Console.Write(elemento + " ");
+        }
+        Console.WriteLine();
+    }
+}
