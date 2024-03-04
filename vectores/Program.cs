215f16b9fe44cabcd9d68ab9ae6be101f1b9e80a
eapch <eapc.gom@gmail.com>
2024-03-04 17:12:34 -0400

    vectores numéricos

--- vectores/Program.cs
+++ vectores/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
  static void Main()
     {
         // Vector de números
-        int[] numeros = new int[] { 1, 2, 3, 4, 5 };
+        int[] numeros = { 1, 2, 3, 4, 5 };
 
         // Vector de letras
-        char[] letras = new char[] { 'a', 'b', 'c', 'd', 'e' };
+        char[] letras = { 'a', 'b', 'c', 'd', 'e' };
 
         // Vector de palabras
-        string[] palabras = new string[] { "Hola", "Mundo", "es", "genial" };
+        string[] palabras = { "Hola", "Mundo", "es", "genial" };
 
         // Imprimir vector de números
         Console.WriteLine("Vector de números:");
@@ -24,8 +25,83 @@ class Program
         // Imprimir vector de palabras
         Console.WriteLine("\nVector con palabras:");
         ImprimirVector(palabras);
+
+
+        //Generar un vector vacío
+        int[] vacio = new int[10]; 
+        Console.WriteLine("\nVector vacío:");
+        ImprimirVector(vacio);
+
+
+        //como obtener el valor de un vector para pasarlo a una variable
+        // la variable debe ser de tipo igual a la de los elementos del vector
+        int valorEnPosicion1 =  numeros[1];
+        Console.WriteLine("\nvector numeros en posición 1:" + valorEnPosicion1);
+
+
+        //podemos modificar un elemento del vector
+        int[] cambio = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("  Vector antes:");
+        ImprimirVector(cambio);
+        cambio[2]=100;
+        Console.WriteLine("  Vector despues:");
+        ImprimirVector(cambio);
+
+
+        //es posible cambiar las posicions de los elementos del array con ayuda de una variable temporal o auxiliar
+        int temp = cambio[3];
+        cambio[3] = cambio[2];
+        cambio[2] = temp;
+        Console.WriteLine("\n Vector con posición intercambiada:");
+        ImprimirVector(cambio);
+
+        //Uso de foreach para recorrer todo el array
+        int suma=0;
+         foreach (int elemento in cambio)
+        {
+            suma += elemento;
+        }
+
+        Console.WriteLine("La suma de los elementos del array es: " + suma);
+
+
+        // haciendo el uso de de algoritmos de recorrido e intercambio de posición, es posible recorrer los elementos de un vector
+               
+        int nuevoElemento = 99;
+        int posicion = 1;
+        for (int i = cambio.Length - 1; i > posicion; i--)
+        {
+            cambio[i] = cambio[i - 1];
+        }
+        cambio[posicion] = nuevoElemento;
+        Console.WriteLine("\nVector después de la inserción:");
+        ImprimirVector(cambio);
+
+
+
+        //Funciones útiles de los vectores
+        ////////////////////////////////////////////////////
+        ///
+
+        //la longitud del vector
+        int longitud = cambio.Length; 
+
+        //copiar los datos de un vector a otro
+        int[] vector2 = new int[5];
+        cambio.CopyTo(vector2, 1);
+
+        //ordenar un vector
+        Array.Sort(cambio);
+
+        //busca el indice o posición de la primer ocurrencia que tengael valor escrito
+        int indice = Array.IndexOf(cambio, 100); 
+
+        //Establece todos los elementos en 0
+        Array.Clear(cambio, 0, cambio.Length);
     }
-    //cambie de idea e hice nuevos cambios de nuevo
+
+
+    
 
     static void ImprimirVector<T>(T[] vector)
     {
---
+++ vectores_ejercicios/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+ static void Main()
+    {
+        int[]vector = generar_vector_aleatorio();
+
+        //realizar los ejercicios peustos en clases
+        ejercicio1(vector);
+
+        ejercicio2();
+    }
+
+    static void ejercicio1(int[]vector){       
+        
+    }
+
+    static void ejercicio2(){
+        
+    }
+
+
+
+    static int[] generar_vector_aleatorio(){
+         // Semilla para el generador de números aleatorios
+        int semilla = 123;
+
+        //se declara un vector de longitud 50
+        int[] vector = new int[50];
+
+        // Inicializar el generador de números aleatorios con la semilla
+        Random random = new Random(semilla);
+
+        // Generar números aleatorios de 2 dígitos y almacenarlos en el vector
+        for (int i = 0; i < 50; i++)
+        {
+            vector[i] = random.Next(10, 99 + 1);
+        }    
+
+        return vector;
+    }
+
+
+
+    static void ImprimirVector<T>(T[] vector)
+    {
+        foreach (T elemento in vector)
+        {
+            Console.Write(elemento + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+
+
+}
